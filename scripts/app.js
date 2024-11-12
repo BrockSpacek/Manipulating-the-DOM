9030a03f3993cdb3bb66fa2ecc4f696ab0eb4f84
BrockSpacek <brockspacek1@gmail.com>
2024-11-12 13:08:37 -0800

    Added Click functions

--- scripts/app.js
+++ scripts/app.js
@@ -23,3 +23,68 @@ let changeElement8 = document.getElementById("changeElement8");
 let changeElement9 = document.getElementById("changeElement9");
 let changeElement10 = document.getElementById("changeElement10");
 
+let changeRule1 = document.getElementById("changeRule1");
+let changeRule2 = document.getElementById("changeRule2");
+let changeRule3 = document.getElementById("changeRule3");
+
+
+
+
+
+btnChange1.addEventListener("click", function(event){
+
+    changeRule1.innerText = "You are such a rule breaker";
+    changeRule2.innerText = "Maybe you should stick to actually playing with the buttons below";
+    changeRule3.innerText = "You might get lost young one";
+
+});
+
+btnChange2.addEventListener("click", function(event){
+
+     
+});
+
+btnChange3.addEventListener("click", function(event){
+
+   
+});
+
+btnChange4.addEventListener("click", function(event){
+
+    
+});
+
+btnChang5.addEventListener("click", function(event){
+
+    
+});
+
+btnChange6.addEventListener("click", function(event){
+
+   
+});
+
+btnChange7.addEventListener("click", function(event){
+
+     
+});
+
+btnChange8.addEventListener("click", function(event){
+
+    
+});
+
+btnChange9.addEventListener("click", function(event){
+
+   
+});
+
+btnChange10.addEventListener("click", function(event){
+
+    
+});
+
+btnChange11.addEventListener("click", function(event){
+
+   
+});
