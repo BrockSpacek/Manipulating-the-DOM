5b9c1ba6bff094449c1da9ede4309e67b5c0444c
BrockSpacek <brockspacek1@gmail.com>
2024-11-12 12:31:59 -0800

    Added Btn Change ID's

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,12 @@
 // alert();
+
+let btnChange1 = document.getElementById("btnChange1");
+let btnChange2 = document.getElementById("btnChange2");
+let btnChange3 = document.getElementById("btnChange3");
+let btnChange4 = document.getElementById("btnChange4");
+let btnChange5 = document.getElementById("btnChange5");
+let btnChange6 = document.getElementById("btnChange6");
+let btnChange7 = document.getElementById("btnChange7");
+let btnChange8 = document.getElementById("btnChange8");
+let btnChange9 = document.getElementById("btnChange9");
+let btnChange10 = document.getElementById("btnChange10");
