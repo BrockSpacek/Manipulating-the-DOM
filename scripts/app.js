181816b727c337da5175a757a1fb323fa1c99a5f
BrockSpacek <brockspacek1@gmail.com>
2024-11-12 12:51:05 -0800

    Added Rules Button and section

--- scripts/app.js
+++ scripts/app.js
@@ -10,3 +10,14 @@ let btnChange7 = document.getElementById("btnChange7");
 let btnChange8 = document.getElementById("btnChange8");
 let btnChange9 = document.getElementById("btnChange9");
 let btnChange10 = document.getElementById("btnChange10");
+
+let changeElement1 = document.getElementById("changeElement1");
+let changeElement2 = document.getElementById("changeElement2");
+let changeElement3 = document.getElementById("changeElement3");
+let changeElement4 = document.getElementById("changeElement4");
+let changeElement5 = document.getElementById("changeElement5");
+let changeElement6 = document.getElementById("changeElement6");
+let changeElement7 = document.getElementById("changeElement7");
+let changeElement8 = document.getElementById("changeElement8");
+let changeElement9 = document.getElementById("changeElement9");
+let changeElement10 = document.getElementById("changeElement10");
